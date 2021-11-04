167b2d60963dfb10116379ccb4fc91236840b0f3
David Anusontarangkul <anusontarangkul.d@gmail.com>
2021-11-03 17:33:25 -0700

    add tests for Counter

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 
 import './App.css';
 
+import Counter from './Counter/Counter'
+
 function App() {
   return (
     <div className="App">
-      <h1>Test</h1>
+      <Counter />
     </div>
   );
 }
